48ce8ee2060301fd2d13d603b3e31796ebeb32b8
Federico Nigro <federico@debfed>
2023-07-29 18:42:36 +0200

    creation of the server running on Debian

---
+++ Debian server/app.py
@@ -0,0 +1,29 @@
+from flask import Flask, render_template, request, url_for, jsonify
+
+app = Flask(__name__)
+# used for checking if the server is available 
+@app.route('/')
+def testConnection():
+    return 'Server is running'
+# POST request test 
+@app.route('/tests/endpoint', methods=['POST'])
+def endpoint():
+    input_json = request.get_json(force=True) 
+    # force=True, above, is necessary if another developer 
+    # forgot to set the MIME type to 'application/json'
+    print('data from client:', input_json)
+    dictToReturn = {'answer':42}
+    return jsonify(dictToReturn)
+# UPLOAD request test
+@app.route('/tests/upload', methods = ['GET', 'POST'])
+def upload_file():
+   if request.method == 'POST':
+      print('begin')
+      f = request.files['upload']
+      f.save(f.filename)
+      print('file uploaded successfully')
+      return 'file uploaded successfully'
+ 
+if __name__ == '__main__':
+    app.run(host="0.0.0.0")
+
