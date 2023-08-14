3894209d892507fcdfc14e21f88069265dc0eb48
Federico Nigro <federico@debfed>
2023-08-14 19:06:35 +0200

    first implementation of the sensor selection page

--- Debian server/static/js/main.js
+++ Debian server/static/js/main.js
@@ -88,26 +88,29 @@
     
     $("#dateFilters").click(function() {
         $("#dashboardContent").hide()
+        $("#filterSensorsSelection").hide()
+        $("#contextFiltersButtons").show()
         $("#filterDateSelection").fadeIn('slow')
+    })
+    $("#sensorsFilters").click(function() {
+        $("#dashboardContent").hide()
+        $("#filterDateSelection").hide()
+        $("#contextFiltersButtons").show()
+        $("#filterSensorsSelection").fadeIn('slow')
     })
 
     $("#backBtn").click(function() {
         $("#filterDateSelection").hide()
+        $("#filterSensorsSelection").hide()
         $("#dashboardContent").fadeIn('slow')
+        $("#contextFiltersButtons").hide()
     })
 
     $(document).ready(function() {
         $("#dashboardContent").show()
         $("#filterDateSelection").hide()
-        // Data Picker Initialization
-        $('#datetimepicker5').datetimepicker({
-            defaultDate: "11/1/2013",
-            disabledDates: [
-                moment("12/25/2013"),
-                new Date(2013, 11 - 1, 21),
-                "11/22/2013 00:53"
-            ]
-        });
+        $('#filterSensorsSelection').hide()
+        $("#contextFiltersButtons").hide()
     })
     
 })(jQuery);
