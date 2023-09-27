a3d3561a60b8dc534e656cd6f85920652ba4a1e1
Federico Nigro <federico@debfed>
2023-09-27 23:16:57 +0200

    working on a new calibration integrating the ppm concentrations of previous iteration

--- Debian server/MQCalib.py
+++ Debian server/MQCalib.py
@@ -117,17 +117,17 @@ class CalcPPM():
             if(RL2Tmp != None):
                 RL2Tmp = (RL2Tmp + RL2RH) / 2
             else: RL2Tmp = RL2RH
-        #if(self.extras['ppmRef'] != None):
-        #    refPPM = self.extras['ppmRef']
-        #    # calculation on fixed values at given ppm 
-        #    RL1ppm = (self.RL1 * currPPM) / refPPM
-        #    RL2ppm = (self.RL2 * currPPM) / refPPM
-        #    if(RL1Tmp != None):
-        #        RL1Tmp = (RL1Tmp + RL1ppm) / 2
-        #    else: RL1Tmp = RL1ppm
-        #    if(RL2ppm != None):
-        #        RL2ppm = (RL2ppm + RL2ppm) / 2
-        #    else: RL2Tmp = RL2ppm 
+        if(self.extras['ppmRef'] != None):
+            refPPM = self.extras['ppmRef']
+            # calculation on fixed values at given ppm 
+            RL1ppm = (self.RL1 * currPPM) / refPPM
+            RL2ppm = (self.RL2 * currPPM) / refPPM
+            if(RL1Tmp != None):
+                RL1Tmp = (RL1Tmp + RL1ppm) / 2
+            else: RL1Tmp = RL1ppm
+            if(RL2ppm != None):
+                RL2ppm = (RL2ppm + RL2ppm) / 2
+            else: RL2Tmp = RL2ppm 
         # recalibration if calculated values are not null
         if(RL1Tmp != None and RL2Tmp != None):
             self.logRL1 = RL1Tmp
@@ -461,8 +461,8 @@ def calculateCurrentPPM(RS, usedR0, sensorName, currT, currRH):
     # STEP4: calculation of the first term equation
     calculusObj['eqFirstTerm'] = (1 / calculusObj['curvCoeff']) * (calculusObj['logRL'] - calculusObj['RL1Log'])
 
-    # STEP5: calculation of the second term equation: this is directly the ppm1
-    calculusObj['eqSecondTerm'] = calculusObj['ppm1Log']
+    # STEP5: calculation of the second term equation: this is directly the current ppm 
+    calculusObj['eqSecondTerm'] = calculusObj['currPPM']
 
     # STEP6: calculus of the PPM (log and then pow)
     logPPMx = calculusObj['eqFirstTerm'] + calculusObj['eqSecondTerm']
